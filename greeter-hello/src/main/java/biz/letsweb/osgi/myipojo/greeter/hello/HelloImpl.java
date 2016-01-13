1bbdc3cf14836b26e40c8be24e1ff8da3baadeb5
Tomasz Kuśmierczyk <tomasz.kusmierczyk@mobica.com>
2016-01-13 14:29:01 +0100

    initial working version

--- greeter-hello/src/main/java/biz/letsweb/osgi/myipojo/greeter/hello/HelloImpl.java
+++ greeter-hello/src/main/java/biz/letsweb/osgi/myipojo/greeter/hello/HelloImpl.java
@@ -2,6 +2,7 @@ package biz.letsweb.osgi.myipojo.greeter.hello;
 
 import biz.letsweb.osgi.myipojo.api.greeter.Hello;
 import org.apache.felix.ipojo.annotations.Component;
+import org.apache.felix.ipojo.annotations.Instantiate;
 import org.apache.felix.ipojo.annotations.Provides;
 
 /**
@@ -9,6 +10,7 @@ import org.apache.felix.ipojo.annotations.Provides;
  * @author toks
  */
 @Component
+@Instantiate
 @Provides
 public class HelloImpl implements Hello {
 
--- greeter-morning/src/main/java/biz/letsweb/osgi/myipojo/greeter/morning/MorningGreeter.java
+++ greeter-morning/src/main/java/biz/letsweb/osgi/myipojo/greeter/morning/MorningGreeter.java
@@ -1,22 +1,30 @@
 package biz.letsweb.osgi.myipojo.greeter.morning;
 
 import biz.letsweb.osgi.myipojo.api.greeter.Hello;
+import org.apache.felix.ipojo.annotations.Component;
+import org.apache.felix.ipojo.annotations.Instantiate;
+import org.apache.felix.ipojo.annotations.Invalidate;
+import org.apache.felix.ipojo.annotations.Requires;
+import org.apache.felix.ipojo.annotations.Validate;
 
 /**
  *
  * @author toks
  */
+@Component
+@Instantiate
 public class MorningGreeter implements Runnable {
 
     /**
      *  Delay between two invocations.
      */
-    private static final int DELAY = 10000;
+    private static final int DELAY = 5000;
 
     /**
      * Hello services.
      * Injected by the container.
      * */
+    @Requires
     private Hello[] hellos;// = new ArrayList<>();
 //    private List<Hello> hellos = new ArrayList<>();
 
@@ -27,13 +35,14 @@ public class MorningGreeter implements Runnable {
 
     @Override
     public void run() {
-        System.out.println("running ...");
+        System.out.println("runnings ...");
         while (!end) {
             try {
                 invokeHelloServices();
                 Thread.sleep(DELAY);
             } catch (InterruptedException ie) {
                 /* will recheck end */
+                System.out.println(ie);
             }
         }
     }
@@ -42,14 +51,17 @@ public class MorningGreeter implements Runnable {
      * Invoke hello services.
      */
     public void invokeHelloServices() {
+        System.out.println("number of hellos: " + hellos.length + " " + System.currentTimeMillis() + " ");
         for (Hello h : hellos) {
-            h.sayHello("world");
+            final String greeting = h.sayHello("world");
+            System.out.print(greeting);
         }
     }
 
     /**
      * Starting.
      */
+    @Validate
     public void starting() {
         System.out.println("starting .... ");
         Thread thread = new Thread(this);
@@ -60,6 +72,7 @@ public class MorningGreeter implements Runnable {
     /**
      * Stopping.
      */
+    @Invalidate
     public void stopping() {
         System.out.println("stopping .... ");
         end = true;
